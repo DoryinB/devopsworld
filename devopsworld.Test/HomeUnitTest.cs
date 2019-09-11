e733657a1ca3e928de7e48c34d5d77cd7e189d5f
doryin.bedford <doryin.bedford@outlook.com>
2019-09-11 13:24:44 -0500

    Katchup

---
+++ devopsworld.Test/HomeUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using devopsworld.Client.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace devopsworld.Test
+{
+  public class HomeUnitTest
+  {
+    [Fact]
+    public void Test_Index()
+    {
+      var sut = new HomeController();
+      var view = sut.Index(); //subject under test
+
+      Assert.NotNull(view);
+      Assert.IsType<ViewResult>(view);
+    }
+  
+    [Fact]
+    public void Test_Privacy()
+    {
+      var sut = new HomeController();
+      var view = sut.Privacy(); //subject under test
+
+      Assert.NotNull(view);
+    }
+  }
+}
